2e1814500c3ddee4f5e4d7c7f62aa9874372a02c
Lachlan Connan <lachlan@connan.org>
2017-08-14 13:45:27 +0800

    changes

---
+++ getToken.js
@@ -0,0 +1,10 @@
+function discordToken() {
+    return "Discord Token";
+}
+
+function twitchToken() {
+    return "Twitch Token";
+}
+
+module.exports.discordToken = discordToken;
+module.exports.twitchToken = twitchToken;

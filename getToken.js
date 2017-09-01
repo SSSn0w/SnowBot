28fb14cb47bfc3ea42afa3680c2a3302cd777cc9
Lachlan Connan <lachlan@connan.org>
2017-09-01 21:43:07 +0800

    added ability to add commands if authorised

--- getToken.js
+++ getToken.js
@@ -1,9 +1,9 @@
 function discordToken() {
-    return "Discord Token";
+    return "MzQ0Mjg1NjgxMTgwMzQ0MzIx.DIr0-A.1zdE_mj47h4ZweTRvzTuPWIH5yk";
 }
 
 function twitchToken() {
-    return "Twitch Token";
+    return "oauth:tzk06pl0n5wn4ds5laeqvz6d6s6gcy";
 }
 
 module.exports.discordToken = discordToken;

7fa81adbf25e567779e2107ea3b71e76526b5eed
Lachlan Connan <lachlan@connan.org>
2018-08-20 19:26:21 +0800

    changes

--- SnowBot/src/main/java/snowbot/SnowBot.java
+++ SnowBot/src/main/java/snowbot/SnowBot.java
@@ -9,7 +9,6 @@ import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import javax.security.auth.login.LoginException;
 
 public class SnowBot {
-    private static final double VERSION = 1.5;
     public static JDA jda;
     
     public static void main(String[] args) throws LoginException, RateLimitedException, InterruptedException {
